45852f79bcd1d1dd4dd45ebfe2dc7bf8cf439fe9
MJ Neto <mauro.jose.neto@gmail.com>
2018-07-08 12:46:52 +0100

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+/* 
+Comentário de varias linhas;
+ */
+
+//Comentário de uma linha;
+
+/* sintaxe jquery;
+
+$(selector).action()
+*/ 
+
+
+/*Função jquery carregada automaticamente logo após o html;
+
+    $(document).ready(function(){
+      console.log('Ready disparado');
+    });
+
+(function(){
+   // jQuery methods go here...
+});
+
+
+*/
+
+/*
+Notas importantes:
+A callback function is executed after the current effect is finished.
+*/
